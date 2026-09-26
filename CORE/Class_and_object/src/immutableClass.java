41b1e314fbb169aeb6af40bafcefb3b37264d03d
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-09-27 01:14:00 +0530

    immutable class

---
+++ CORE/Class_and_object/src/immutableClass.java
@@ -0,0 +1,9 @@
+class student{
+    final   String name;
+    final  int scholarNumber;
+    student(String name,int scholarNumber){
+        this.name=name;
+        this.scholarNumber=scholarNumber;
+    }
+}
+// As we can only declare name of a student only at time of object creation and can't modify it in future
